762f2838c41bbf4f6542aa56b2bfe073a6ffa62e
yohannes belete <yohannes@kali.kali>
2026-02-04 10:47:25 -0500

    updated commit

--- singly_linkedList.py
+++ singly_linkedList.py
@@ -1,1 +1,23 @@
-print("hello world")
+class Node:
+  def __init__(self, val):
+    self.value = val
+    self.next = None
+    
+class SinglyLinkedList:
+  def __init__(self):
+    self.head = None
+    
+  def insertAtBeginning(self, value):
+    node = Node(value)
+    
+    if self.head is None:
+      self.head = node
+      return
+    node.next = self.head
+    self.head = node
+    
+    
+    
+linkedList = SinglyLinkedList()
+linkedList.insertAtBeginning(4)
+linkedList.insertAtBeginning(4)
