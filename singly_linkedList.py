1583912cca6379893a2df732670cc25504d7f9fc
yohannes belete <yohannes@kali.kali>
2026-02-04 11:25:28 -0500

    updated commit

--- singly_linkedList.py
+++ singly_linkedList.py
@@ -16,8 +16,28 @@ class SinglyLinkedList:
     node.next = self.head
     self.head = node
     
+  def insertAtEnd(self, value):
+    node = Node(value)
+    if self.head is node:
+      self.head = node
+    cur = self.head
+    while cur.next is not None:
+      cur = cur.next
+    cur.next = node
+  
+  def display(self):
+    if self.head is None:
+      return
+    cur = self.head
+    while cur is not None:
+      print(cur.value, end=" --> ")
+      cur = cur.next
     
     
 linkedList = SinglyLinkedList()
 linkedList.insertAtBeginning(4)
-linkedList.insertAtBeginning(4)
+linkedList.insertAtBeginning(5)
+linkedList.insertAtEnd(6)
+linkedList.insertAtEnd(1)
+linkedList.insertAtBeginning(9)
+linkedList.display()
